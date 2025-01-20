28b63b9e82de2f13a3897d7f21afe78521248288
HamidReza <stormystormy64@gmail.com>
2025-01-20 19:16:45 +0330

    Edit

--- src/pages/StartMenu.tsx
+++ src/pages/StartMenu.tsx
@@ -17,11 +17,11 @@ const StartMenu = () => {
     return (
  
         <div className='start-menu-bg w-full h-full center-row'>
-            <button onClick={() => play()}>Click Me!!</button>
-            {/* <Link to={(+GetLocalStorage("level")) < 4 ? "/select-plants" : ""}><img src={ ImagesSource("/startMenu/startAdventure.png") } alt="" className='absolute right-16 top-16 brightness' /></Link>
+            
+            <Link to={(+GetLocalStorage("level")) < 4 ? "/select-plants" : ""}><img src={ ImagesSource("/startMenu/startAdventure.png") } alt="" className='absolute right-16 top-16 brightness' /></Link>
             <div className='w-60 h-6 absolute left-14 top-[84px] px-2 text-white truncate'>{ userName !== "" ? userName : "UserName"}</div>
 
-            <UserNameMenu setUsername={setUsername} /> */}
+            <UserNameMenu setUsername={setUsername} />
 
         </div>
     )
